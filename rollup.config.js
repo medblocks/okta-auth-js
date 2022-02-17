c50854dfe8aa1eecf138991f3bcc7f639ad50aba
Shuo Wu <wushuo2010@gmail.com>
2022-02-17 13:50:47 -0500

    adjust rollup config for development

--- rollup.config.js
+++ rollup.config.js
@@ -22,60 +22,59 @@ const makeExternalPredicate = () => {
 };
 
 const extensions = ['js', 'ts'];
-
 const external = makeExternalPredicate();
-const commonPlugins = [
-  replace({
-    'SDK_VERSION': JSON.stringify(pkg.version),
-    'global.': 'window.',
-    preventAssignment: true
-  }),
-  alias({
-    entries: [
-      { find: /.\/node$/, replacement: './browser' }
-    ]
-  }),
-  typescript({
-    // eslint-disable-next-line node/no-unpublished-require
-    typescript: require('typescript'),
-    tsconfigOverride: {
-      compilerOptions: {
-        sourceMap: true,
-        target: 'ES2017', // skip async/await transpile
-        declaration: false
-      }
-    }
-  }),
-  cleanup({ 
-    extensions,
-    comments: 'none'
-  }),
-  license({
-    banner: {
-      content: {
-        file: path.join(__dirname, 'scripts', 'license-template'),
-      }
-    }
-  })
-];
 
 export default [
   {
     input: 'lib/index.ts',
     external,
     plugins: [
-      ...commonPlugins,
-      babel({
+      replace({
+        'SDK_VERSION': JSON.stringify(pkg.version),
+        'global.': 'window.',
+        preventAssignment: true
+      }),
+      alias({
+        entries: [
+          { find: /.\/node$/, replacement: './browser' }
+        ]
+      }),
+      typescript({
+        // eslint-disable-next-line node/no-unpublished-require
+        typescript: require('typescript'),
+        tsconfigOverride: {
+          compilerOptions: {
+            sourceMap: true,
+            target: 'ES2017', // skip async/await transpile
+            declaration: false
+          }
+        }
+      }),
+      // not add @babel/runtime import for development
+      (process.env.NODE_ENV !== 'development' && babel({
         babelHelpers: 'runtime',
         presets: [
           '@babel/preset-env'
-          
         ],
         plugins: [
-          '@babel/plugin-transform-runtime'
+          // https://babeljs.io/docs/en/babel-plugin-transform-runtime#corejs
+          ['@babel/plugin-transform-runtime', { 
+            corejs: 3 
+          }],
         ],
         extensions
+      })),
+      cleanup({ 
+        extensions,
+        comments: 'none'
       }),
+      license({
+        banner: {
+          content: {
+            file: path.join(__dirname, 'scripts', 'license-template'),
+          }
+        }
+      })
     ],
     output: [
       {
@@ -84,14 +83,15 @@ export default [
         exports: 'named',
         sourcemap: true
       },
-      {
+      // not emit test bundle for development
+      (process.env.NODE_ENV !== 'development' && {
         // generate ems bundle for jest test, ".mjs" extension should be used
         // this bundle is excluded from the release package
         format: 'esm',
         file: 'build/bundles-for-validation/esm/index.mjs',
         exports: 'named',
         sourcemap: true
-      }
+      })
     ]
   }
 ];
